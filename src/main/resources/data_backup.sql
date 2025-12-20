8ab962f1bad46e1801f16f60c8724a30fc45600a
unknown <yyun01964gmail.com>
2025-12-20 23:13:07 +0900

    Clean up branch for new features

--- src/main/resources/data_backup.sql
+++
@@ -1,8 +0,0 @@
--- 이 파일은 애플리케이션 시작 시 자동으로 실행됩니다.
-
--- User 테이블에 테스트용 데이터 2건을 삽입합니다.
-/*INSERT INTO user (id, name, age) VALUES (1, '홍길동', 30);
-INSERT INTO user (id, name, age) VALUES (2, '김철수', 25); */
-
--- 필요하다면 다른 테이블의 데이터도 추가할 수 있습니다.
--- INSERT INTO product (id, name, price) VALUES (101, '노트북', 1500000);
